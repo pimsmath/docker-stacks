c077a2e3a4666a9c7c8db8b4a749453d81415188
Ian Allison <iana@pims.math.ca>
2017-11-02 11:40:03 -0700

    Fixed systemuser.sh for new versions of dockerspawner
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- pims-notebook/systemuser.sh
+++ pims-notebook/systemuser.sh
@@ -14,13 +14,6 @@ then
 fi
 
 sudo -E PATH="${CONDA_DIR}/bin:$PATH" -u $USER jupyterhub-singleuser \
-  --port=8888 \
-  --ip=0.0.0.0 \
-  --user=$JPY_USER \
-  --cookie-name=$JPY_COOKIE_NAME \
-  --base-url=$JPY_BASE_URL \
-  --hub-prefix=$JPY_HUB_PREFIX \
-  --hub-api-url=$JPY_HUB_API_URL \
   ${notebook_arg} \
   $@
 
