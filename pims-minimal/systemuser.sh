e46a10c82e43e361d6222f5ef52442c277b97e6b
Joe Topjian <joe@topjian.net>
2018-05-04 21:31:19 +0000

    $USER and $USER_ID fixes

--- pims-minimal/systemuser.sh
+++ pims-minimal/systemuser.sh
@@ -1,19 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-if getent passwd $USER_ID > /dev/null ; then
-  echo "$USER ($USER_ID) exists"
+
+if [[ -z $NB_USER ]]; then
+  echo "NB_USER is not set"
+  exit 1
+fi
+
+if [[ -z $NB_UID ]]; then
+  echo "NB_UID is not set"
+  exit 1
+fi
+
+if getent passwd $NB_UID > /dev/null ; then
+  echo "$NB_USER ($NB_UID) exists"
 else
-  echo "Creating user $USER ($USER_ID)"
-  useradd -u $USER_ID -s $SHELL $USER
+  echo "Creating user $NB_USER ($NB_UID)"
+  useradd -u $NB_UID -s $SHELL $NB_USER
 fi
 
 notebook_arg=""
-if [ -n "${NOTEBOOK_DIR:+x}" ]
-then
+if [[ -n "${NOTEBOOK_DIR:+x}" ]]; then
     notebook_arg="--notebook-dir=${NOTEBOOK_DIR}"
 fi
 
-sudo -E PATH="${CONDA_DIR}/bin:$PATH" -E LD_LIBRARY_PATH="${CONDA_DIR}/lib" -u $USER jupyterhub-singleuser \
-  ${notebook_arg} \
-  $@
-
+if [[ ! -z $JUPYTERHUB_API_TOKEN ]]; then
+  sudo -E PATH="${CONDA_DIR}/bin:$PATH" -E LD_LIBRARY_PATH="${CONDA_DIR}/lib" -u $NB_USER jupyterhub-singleuser \
+    ${notebook_arg} \
+    $@
+else
+  . /usr/local/bin/start.sh jupyter notebook $*
+fi
