c82ef2a89e5299c797bd39f9194e00d53a24a571
Cloud User <ptty2u@vse.syzygy.ca>
2018-09-25 20:35:36 +0000

    Drop DataArrays for the moment, seems to be broken
    Signed-off-by: Cloud User <ptty2u@vse.syzygy.ca>

--- pims-julia/package-installs.jl
+++ pims-julia/package-installs.jl
@@ -1,3 +1,3 @@
 import Pkg
 
-Pkg.pkg"add BinDeps Cairo Calculus Clustering Clp Colors DataArrays DataFrames DataFramesMeta Dates DecisionTree Distributions Distances GLM HDF5 HypothesisTests IJulia Ipopt JSON KernelDensity Lazy MLBase MultivariateStats NLopt Optim ODE PDMats PGFPlots Plots PyCall PyPlot QuantEcon RDatasets SQLite Stan StatsBase Sundials ZipFile ZMQ; precompile"
+Pkg.pkg"add BinDeps Cairo Calculus Clustering Clp Colors DataFrames DataFramesMeta Dates DecisionTree Distributions Distances GLM HDF5 HypothesisTests IJulia Ipopt JSON KernelDensity Lazy MLBase MultivariateStats NLopt Optim ODE PDMats PGFPlots Plots PyCall PyPlot QuantEcon RDatasets SQLite Stan StatsBase Sundials ZipFile ZMQ; precompile"
