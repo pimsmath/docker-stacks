b869d301db05f8b5f91626f4a999497ddb59c948
VSE-Syzygy-SSH <soodarnav01@gmail.com>
2018-11-05 18:18:09 +0000

    quickfixes to pin jupyterlab and bokeh
    Signed-off-by: VSE-Syzygy-SSH <soodarnav01@gmail.com>

--- pims-julia/startup.jl
+++ pims-julia/startup.jl
@@ -1,4 +1,4 @@
 if !isdir(ENV["HOME"] * "/.julia/environments") && isdir("/opt/julia/environments")
     isdir(ENV["HOME"] * "/.julia") ? true : mkdir(ENV["HOME"] * "/.julia")
-    cp("/opt/julia-1.0/environments", ENV["HOME"] * "/.julia/environments")
+    cp("/opt/julia/environments", ENV["HOME"] * "/.julia/environments")
 end
